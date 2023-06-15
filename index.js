8ffdb3fde5da1aa2f612a1682e0b79123c32d0b3
devsecopscloset <106809417+devsecopscloset@users.noreply.github.com>
2023-06-15 15:22:54 +0530

    Check-3

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ var express = require('express');
 var app = express();
 
 app.get('/', function (req, res) {
-  res.send('<h1>Hello Closetserver Final</h1>');
+  res.send('<h1>Hello Closetserver Checking</h1>');
 
 });
 
